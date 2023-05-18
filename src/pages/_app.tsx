908cb544bbc889845f6ed61c3fa23cb77dd10dcd
krlslman <selmankorall@gmail.com>
2023-05-18 14:34:07 +0300

    auth0 is implemented

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
